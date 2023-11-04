909deab3b3a514389d4006785c1f205af7d0d17f
collinsln <collangat@gmail.com>
2023-11-04 17:24:03 +0300

    onClick e on bots

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import React from "react";
 import Bots from "./Bots";
+import "./assets/App.css"
 
 
 
 function App() {
 const[data,setData]=useState([])
+const[selectedbot,setSelectedbot]=useState("")
 //const[avatar_url,setAvatar]=useState("")
 
 
@@ -18,14 +20,19 @@ const[data,setData]=useState([])
 
 
 
+//const handleselect=(e)=> console.log(e.target.value)
+ //setSelectedbot(e.target.value)
+//}
 
 
   return (
-    <div>
+    
+    <div className='container'>
       <h1>Bots</h1>
       
+      
         {data.map(bots=>(
-        <Bots key={bots.id} name={bots.name} avatar_url={bots.avatar_url}  health={bots.health} damage={bots.damage} armor={bots.armor} bot_class={bots.bot_class}/>
+        <Bots  key={bots.id} name={bots.name} avatar_url={bots.avatar_url}  health={bots.health} damage={bots.damage} armor={bots.armor} bot_class={bots.bot_class}/>
        
         ))
        
--- src/Bots.jsx
+++ src/Bots.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
+import pp from './assets/App.css'
 
 function Bots({name,health,damage,armor,bot_class,avatar_url,catchphrase,}) {
+  const clicked=()=>{console.log(name)}
+  
+  
   return (
-    <div>
-      <h1></h1>
+    <>
+    
+     <div className='container'>
+      
+
+     <div className='botgrid' onClick={clicked}>
+      <img src={avatar_url}/>
+
         <p>{name}</p>
         <p>{health}</p>
         <p>{damage}</p>
         <p>{armor}</p>
         <p>{bot_class}</p>
-        <img src={avatar_url}/>
-    </div>
+        
+       </div>
+       
+        
+     </div>
+     
+    </>
   )
 }
 
